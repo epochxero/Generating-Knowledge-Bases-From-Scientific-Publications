cb30ad65989b333c4eadba060e6ae92ce34328fc
TyJK <TJKolody@gmail.com>
2019-07-24 00:57:10 -0400

    Update and rename ElsevierMultiThreadScraper.py to ElsevierScraper.py
    Added file split and merge helper functions, added a soft check for university VPN connection.

--- Data/ElsevierScraper.py
+++ Data/ElsevierScraper.py
@@ -14,8 +14,36 @@ to grab 100 articles (the progress counter appears every 100 articles). 18 - 20 
 issues) but 20 - 25 seconds is likely less likely safer. 
 """
 
+def file_split(input_file, chunk_size=50000, index=False):
+    """
+    Splits CSV files into smaller files for easier reading
 
-def mp_handler(DOI_file, output_file, mapped_function, num_processes=4):
+    :param input_file: name of the input file
+    :param chunk_size: number of rows per chunk
+    :param index: if True, creates an index column
+    :return: void: returns multiple CSV files
+    """
+
+    for i, chunk in enumerate(pd.read_csv(input_file, chunksize=chunk_size, error_bad_lines=False)):
+        chunk.to_csv('{}{}.csv'.format(input_file.split(".")[0], i), index=index)
+
+
+def file_merge(file_list, output_file, index=False):
+    """
+    Merge a list of CSVs into one file
+
+    :param file_list: list of file names
+    :param output_file: name of aggregate file
+    :param index: if True, creates an index column
+    :return: void: outputs to CSV
+    """
+    for file_name in file_list:
+        file = pd.read_csv(file_name, error_bad_lines=False, header=True)
+        file.to_csv(open(output_file, 'a', encoding="utf8"), index=index)
+        print("{} added".format(file_name))
+
+
+def mp_handler(DOI_file, output_file, mapped_function, num_processes=2):
     """
     Manages multiprocessing for scraping functions. Takes a list of DOIs and an output file, finds the most recent DOI
     in the output file and iterates through the DOIs from there.
@@ -43,8 +71,9 @@ def mp_handler(DOI_file, output_file, mapped_function, num_processes=4):
                 f.seek(-2, os.SEEK_CUR)
             last_line = f.readline().decode()
 
-        start = last_line.split(",")[0]
+        start = last_line.split(",")[1]
         print("Most Recent DOI: {}".format(start))
+        print("Most Recent Output: {}".format(last_line))
         start = DOIs.index(start.strip("\n").strip("\r")) + 1
         print("Resume Position: {}".format(start))
         DOIs = DOIs[start:]
@@ -53,7 +82,9 @@ def mp_handler(DOI_file, output_file, mapped_function, num_processes=4):
         print(e)
         if os.path.isfile(output_file):
             print("Invalid DOI, please check the last line of the file to ensure it's written properly")
+
             return
+
         else:
             print("No file exists, initializing file")
             with open(output_file, mode='a', encoding='utf8', newline="") as outputFile:
@@ -71,7 +102,11 @@ def mp_handler(DOI_file, output_file, mapped_function, num_processes=4):
             count += 1
             if count % 100 == 0:
                 print("Total Progress: {}%".format(round(float(count + progress) / total * 100, 4)))
+                print("Most Recent Output: {}".format(result))
+                if "xocs" in result:
+                    print("Your output does not contain the full text. Ensure that you're on a registered university network and run it again")
 
+                    return
     print("Done!")
 
 def ElsevierScraper(client, target_DOI):
@@ -102,6 +137,7 @@ def ElsevierScraper(client, target_DOI):
         # Could save this to a separate file but it's easier to search for NA and make a new sublist after the fact
         print("Error: couldn't read {}.".format(target_DOI))
         return [target_DOI, "NA", "NA","NA","NA","NA"]
+
 
 
 if __name__ == "__main__":
